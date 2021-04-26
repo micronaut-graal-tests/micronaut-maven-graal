23ce6f6c699890d12a33995971764fe95db63d65
Iván López <lopezi@objectcomputing.com>
2021-04-26 13:33:05 +0200

    Add controller and service

---
+++ src/main/java/example/micronaut/HelloController.java
@@ -0,0 +1,26 @@
+package example.micronaut;
+
+import io.micronaut.core.version.VersionUtils;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.reactivex.Single;
+
+@Controller("/")
+public class HelloController {
+
+    private final GreetingService greetingService;
+
+    public HelloController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    @Get("/hello/{name}")
+    public Single<Greeting> sayHi(String name) {
+        return greetingService.sayHi(name);
+    }
+
+    @Get("/version")
+    public String version() {
+        return VersionUtils.MICRONAUT_VERSION;
+    }
+}
